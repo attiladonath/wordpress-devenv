ecd2109761b0a6f9ee60c102b351792304dda442
Attila Donath <contact@attiladonath.com>
2016-07-19 17:25:22 +0200

    Basic environment with Apache 7.0 and MariaDB 10.1.
    Expects Wordpress to be under ../docroot and creates a default wp-config.php if the file is not present.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # wordpress-devenv
-A boilerplate template for creating a simple Wordpress development environment with Docker.
+
+A boilerplate template for creating a simple Wordpress development environment
+with Docker. It uses Apache 7.0 and MariaDB 10.1. Tested with WordPress 4.5.3.
+
+**Note: This template creates a simple environment for quick local development.
+It is not secure by any means so do not use it for public-facing solutions!**
+
+The data in the database is persistent as it is on a Docker volume.
+
+The Apache docroot is also a volume which is binded to `../docroot`.
+
+`tree`:
+```
+.
+├── wordpress-devenv
+│   ├── [The files in this repo.]
+└── docroot
+    ├── index.php
+    ├── wp-config-sample.php
+    ├── wp-content
+    ├── ...
+    ├── [And other WorPress files.]
+```
+
+## Usage
+
+Initiate the environment: `docker-compose up`
+
+Apache listens on port 80, so you can simply access WordPress under:
+`http://localhost`
+
+Connecting to the database: `mysql -h 127.0.0.1 -u wordpress -pwordpress`
+
+You can remove stopped containers and volumes (deletes the database) with:
+`docker-compose rm -v`
---
+++ webserver/init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ ! -f wp-config.php ]
+then
+  # Create the config file.
+  cp wp-config-sample.php wp-config.php
+
+  # Fill in DB settings according to the database env variables.
+  DB_HOST="$(echo $DATABASE_PORT | sed -e 's|.*://\(.*\):\(.*\)|\1|')"
+  sed -i "s/define('DB_NAME', 'database_name_here');/define('DB_NAME', '$DATABASE_ENV_MYSQL_DATABASE');/g" wp-config.php
+  sed -i "s/define('DB_USER', 'username_here');/define('DB_USER', '$DATABASE_ENV_MYSQL_USER');/g" wp-config.php
+  sed -i "s/define('DB_PASSWORD', 'password_here');/define('DB_PASSWORD', '$DATABASE_ENV_MYSQL_PASSWORD');/g" wp-config.php
+  sed -i "s/define('DB_HOST', 'localhost');/define('DB_HOST', '$DB_HOST');/g" wp-config.php
+
+  # Generate random hash salts.
+  SED_ESCAPE_PATTERN='s/[\/&]/\\&/g'
+  PASSPHRASE_PLACEHOLDER='put your unique phrase here'
+  grep "$PASSPHRASE_PLACEHOLDER" wp-config.php | while read LINE
+  do
+    # The salt is escaped twice because of the followig two sed replacements.
+    SALT=$(openssl passwd -1 $(date +%s) | sed $SED_ESCAPE_PATTERN | sed $SED_ESCAPE_PATTERN)
+    LINE_REPLACEMENT=$(echo $LINE | sed "s/$PASSPHRASE_PLACEHOLDER/$SALT/g")
+    sed -i "s/$LINE/$LINE_REPLACEMENT/g" wp-config.php
+  done
+fi
+
+# Start Apache.
+apache2-foreground
